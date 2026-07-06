990f6a81c8264a63661df5f571b8c6cf78a5df4e
carl-stone <carl.j.stone@vanderbilt.edu>
2026-07-06 07:34:00 -0500

    Avoid ambiguous distribution plot validation formatting

--- R/plot_distribution.R
+++ R/plot_distribution.R
@@ -54,9 +54,10 @@ plot_methylation_distribution <- function(object,
   if (!is(object, "commaData")) {
     stop("'object' must be a commaData object.")
   }
-  if (!is.logical(per_sample) ||
-        length(per_sample) != 1L ||
-        is.na(per_sample)) {
+  invalid_per_sample <- !is.logical(per_sample) ||
+    length(per_sample) != 1L ||
+    is.na(per_sample)
+  if (invalid_per_sample) {
     stop("'per_sample' must be TRUE or FALSE.")
   }
 
